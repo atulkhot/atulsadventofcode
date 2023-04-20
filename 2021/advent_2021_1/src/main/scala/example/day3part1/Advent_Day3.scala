92c6cd50885549bc0d41e3a21c59f90b69c2c7b5
Atul S Khot <atul.khot@gmail.com>
2023-04-20 12:02:15 +0530

    Many improvements - removed an incorrect usage of foldLeft

--- 2021/advent_2021_1/src/main/scala/example/day3part1/Advent_Day3.scala
+++ 2021/advent_2021_1/src/main/scala/example/day3part1/Advent_Day3.scala
@@ -4,7 +4,7 @@ import scala.io.Source
 
 object Advent_Day3 {
   private val dataFile = "./src/main/resources/example/day3part1/example_data.txt"
-//  private val dataFile = "./src/main/resources/example/day3part1/sample.txt"
+  //  private val dataFile = "./src/main/resources/example/day3part1/sample.txt"
 
   val dataList =
     Source.fromFile(dataFile)
@@ -14,18 +14,18 @@ object Advent_Day3 {
   def bitsAtPos(list: List[String], pos: Int): List[Char] =
     list.flatMap(s => List(s(pos)))
 
-  def gammaBitAt(list: List[String], pos: Int): String = {
-    val numOnes = bitsAtPos(list, pos).count((x: Char) => x.toString == "1")
-    val numZeroes = bitsAtPos(list, pos).count((x: Char) => x.toString == "0")
+  private def computeBitsAtPos(list: List[String], pos: Int) = {
+    bitsAtPos(list, pos).partition((x: Char) => x.toString == "1")
+  }
 
-    if (numOnes >= numZeroes) "1" else "0"
+  def gammaBitAt(list: List[String], pos: Int): String = {
+    val (ones, zeroes) = computeBitsAtPos(list, pos)
+    if (ones.size > zeroes.size) "1" else "0"
   }
 
   def epsilonBitAt(list: List[String], pos: Int): String = {
-    val numOnes = bitsAtPos(list, pos).count((x: Char) => x.toString == "1")
-    val numZeroes = bitsAtPos(list, pos).count((x: Char) => x.toString == "0")
-
-    if (numOnes <= numZeroes) "1" else "0"
+    val (ones, zeroes) = computeBitsAtPos(list, pos)
+    if (ones.size > zeroes.size) "0" else "1"
   }
 
   def main(args: Array[String]): Unit = {
@@ -34,12 +34,12 @@ object Advent_Day3 {
 
     val gammaStr = for {
       i <- (0 until charsInBinaryNumber).toList
-      k <- dataList.foldRight("")((elem, acc) => "" + gammaBitAt(dataList, i))
+      k <- gammaBitAt(dataList, i)
     } yield k
 
     val epsilonStr = for {
       i <- (0 until charsInBinaryNumber).toList
-      k <- dataList.foldRight("")((elem, acc) => "" + epsilonBitAt(dataList, i))
+      k <- epsilonBitAt(dataList, i)
     } yield k
 
     val gammaNumber = Integer.parseInt(gammaStr.mkString, 2)
@@ -47,10 +47,5 @@ object Advent_Day3 {
 
     val powerConsumption = gammaNumber * epsilonNumber
     println(powerConsumption)
-//    println(epsilonBitAt(list, 0))
-//    println(epsilonBitAt(list, 1))
-//    println(epsilonBitAt(list, 2))
-//    println(epsilonBitAt(list, 3))
-//    println(epsilonBitAt(list, 4))
   }
 }
