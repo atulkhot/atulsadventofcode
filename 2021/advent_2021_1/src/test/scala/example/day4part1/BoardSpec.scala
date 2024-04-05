50c351c31c4ac697c38174cad3bfe972a154ef12
Atul S Khot <atul.khot@gmail.com>
2024-04-05 12:58:51 +0530

    Using fixtures to remove data duplication

--- 2021/advent_2021_1/src/test/scala/example/day4part1/BoardSpec.scala
+++ 2021/advent_2021_1/src/test/scala/example/day4part1/BoardSpec.scala
@@ -8,14 +8,67 @@ import example.day4part1.Board.BoardState
 import org.scalatest.OptionValues.convertOptionToValuable
 
 class BoardSpec extends AnyFlatSpec with should.Matchers {
-  "A board representation" should "return proper coordinates" in {
-    val boardData = List(
+  def fixture = new {
+    val boardData1 = List(
       14, 21, 17, 24, 4,
       10, 16, 15, 9, 19,
       18, 8, 23, 26, 20,
       22, 11, 13, 6, 5,
       2, 0, 12, 3, 7
     )
+
+    val boardData2 = List(
+      14, 21, 13, 24, 4,
+      13, 16, 15, 9, 19,
+      18, 8, 23, 26, 20,
+      22, 11, 13, 6, 5,
+      2, 0, 12, 3, 4
+    )
+
+    val zerothRowAll14 = List(
+      14, 14, 14, 14, 14,
+      13, 16, 15, 9, 19,
+      18, 8, 23, 26, 20,
+      22, 11, 13, 6, 5,
+      2, 0, 12, 3, 4
+    )
+
+    val zerothColAll14 = List(
+      14, 21, 13, 24, 4,
+      14, 16, 15, 9, 19,
+      14, 8, 23, 26, 20,
+      14, 11, 13, 6, 5,
+      14, 0, 12, 3, 4
+    )
+
+    val listOfBoards = List(
+      List(
+        22, 13, 17, 11, 0,
+        8, 2, 23, 4, 24,
+        21, 9, 14, 16, 7,
+        6, 10, 3, 18, 5,
+        1, 12, 20, 15, 19
+      ),
+      List(
+        3, 15, 0, 2, 22,
+        9, 18, 13, 17, 5,
+        19, 8, 7, 25, 23,
+        20, 11, 10, 24, 4,
+        14, 21, 16, 12, 6
+      ),
+      List(
+        14, 21, 17, 24, 4,
+        10, 16, 15, 9, 19,
+        18, 8, 23, 26, 20,
+        22, 11, 13, 6, 5,
+        2, 0, 12, 3, 7
+      )
+    ).map(boardData => Board(5, 5, boardData))
+  }
+
+  "A board representation" should "return proper coordinates" in {
+    val f = fixture
+    val boardData = f.boardData1
 
     val board = Board(5, 5, boardData)
 
@@ -25,13 +78,8 @@ class BoardSpec extends AnyFlatSpec with should.Matchers {
   }
 
   "A board representation with duplicate numbers" should "return proper coordinates" in {
-    val boardData = List(
-      14, 21, 13, 24, 4,
-      13, 16, 15, 9, 19,
-      18, 8, 23, 26, 20,
-      22, 11, 13, 6, 5,
-      2, 0, 12, 3, 4
-    )
+    val f = fixture
+    val boardData = f.boardData2
 
     val board = Board(5, 5, boardData)
 
@@ -40,18 +88,13 @@ class BoardSpec extends AnyFlatSpec with should.Matchers {
   }
 
   "Marking an element" should "update the coordinates vectors" in {
-    val boardData = List(
-      14, 21, 13, 24, 4,
-      13, 16, 15, 9, 19,
-      18, 8, 23, 26, 20,
-      22, 11, 13, 6, 5,
-      2, 0, 12, 3, 4
-    )
+    val f = fixture
+    val boardData = f.boardData2
 
     val board = Board(5, 5, boardData)
     val initialSize = board.board.size
 
-    val startingState: BoardState[Unit] = Board.markBoardElement(14) // board.markACoordinate
+    val startingState: BoardState[Unit] = Board.markBoardElement(14)
 
     val resultState: Board = startingState.runS(board).value
 
@@ -63,13 +106,8 @@ class BoardSpec extends AnyFlatSpec with should.Matchers {
   }
 
   "Marking an element so a single row" should "is completely marked" in {
-    val boardData = List(
-      14, 14, 14, 14, 14,
-      13, 16, 15, 9, 19,
-      18, 8, 23, 26, 20,
-      22, 11, 13, 6, 5,
-      2, 0, 12, 3, 4
-    )
+    val f = fixture
+    val boardData = f.zerothRowAll14
 
     val board = Board(5, 5, boardData)
     val initialSize = board.board.size
@@ -80,20 +118,15 @@ class BoardSpec extends AnyFlatSpec with should.Matchers {
 
     resultState.rows should contain theSameElementsAs Vector(5, 0, 0, 0, 0)
     resultState.cols should contain only(1)
-    
+
     resultState.board.size should be(initialSize - 1)
 
     resultState.winningBoardElemOpt.isDefined should be(true)
   }
 
   "Marking an element so a single column" should "is completely marked" in {
-    val boardData = List(
-      14, 21, 13, 24, 4,
-      14, 16, 15, 9, 19,
-      14, 8, 23, 26, 20,
-      14, 11, 13, 6, 5,
-      14, 0, 12, 3, 4
-    )
+    val f = fixture
+    val boardData = f.zerothColAll14
 
     val board = Board(5, 5, boardData)
     val initialSize = board.board.size
@@ -113,13 +146,8 @@ class BoardSpec extends AnyFlatSpec with should.Matchers {
   }
 
   "Marking a series of elements so a single row" should "is completely marked" in {
-    val boardData = List(
-      14, 21, 17, 24, 4,
-      10, 16, 15, 9, 19,
-      18, 8, 23, 26, 20,
-      22, 11, 13, 6, 5,
-      2, 0, 12, 3, 7
-    )
+    val f = fixture
+    val boardData = f.boardData1
 
     val board = Board(5, 5, boardData)
 
@@ -150,70 +178,28 @@ class BoardSpec extends AnyFlatSpec with should.Matchers {
   }
 
   "Drawing elements for a set of boards" should "calculate the right score for the winning board" in {
-    val inputBoards = List(
-      List(
-        22, 13, 17, 11, 0,
-        8, 2, 23, 4, 24,
-        21, 9, 14, 16, 7,
-        6, 10, 3, 18, 5,
-        1, 12, 20, 15, 19
-      ),
-      List(
-        3, 15, 0, 2, 22,
-        9, 18, 13, 17, 5,
-        19, 8, 7, 25, 23,
-        20, 11, 10, 24, 4,
-        14, 21, 16, 12, 6
-      ),
-      List(
-        14, 21, 17, 24, 4,
-        10, 16, 15, 9, 19,
-        18, 8, 23, 26, 20,
-        22, 11, 13, 6, 5,
-        2, 0, 12, 3, 7
-      )
-    ).map(boardData => Board(5, 5, boardData))
+    val f = fixture
+    val inputBoards = f.listOfBoards
 
     val result = Board.makeAnElementOfAllBoards(inputBoards, 7)
 
     result.size should be(3)
 
-    val (updatedBoard0, updatedBoard1, updatedBoard2) = (result(0), result(1), result(2))
+    val (updatedBoard0, updatedBoard1, updatedBoard2) = (result.get(0), result.get(1), result.get(2))
 
-    updatedBoard0.rows(2) should be(1)
-    updatedBoard0.cols(4) should be(1)
+    updatedBoard0.value.rows(2) should be(1)
+    updatedBoard0.value.cols(4) should be(1)
 
-    updatedBoard1.rows(2) should be(1)
-    updatedBoard1.cols(2) should be(1)
+    updatedBoard1.value.rows(2) should be(1)
+    updatedBoard1.value.cols(2) should be(1)
 
-    updatedBoard2.rows(4) should be(1)
-    updatedBoard2.cols(4) should be(1)
+    updatedBoard2.value.rows(4) should be(1)
+    updatedBoard2.value.cols(4) should be(1)
   }
 
   "Drawing a series of elements for a set of boards" should "returns one winning board" in {
-    val inputBoards = List(
-      List(
-        22, 13, 17, 11, 0,
-        8, 2, 23, 4, 24,
-        21, 9, 14, 16, 7,
-        6, 10, 3, 18, 5,
-        1, 12, 20, 15, 19
-      ),
-      List(
-        3, 15, 0, 2, 22,
-        9, 18, 13, 17, 5,
-        19, 8, 7, 25, 23,
-        20, 11, 10, 24, 4,
-        14, 21, 16, 12, 6
-      ),
-      List(
-        14, 21, 17, 24, 4,
-        10, 16, 15, 9, 19,
-        18, 8, 23, 26, 20,
-        22, 11, 13, 6, 5,
-        2, 0, 12, 3, 7
-      )
-    ).map(boardData => Board(5, 5, boardData))
+    val f = fixture
+    val inputBoards = f.listOfBoards
 
     val drawnNumbers = List(7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16, 13, 6, 15, 25, 12, 22, 18, 20, 8, 19, 3, 26, 1)
 
